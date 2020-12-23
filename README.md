9e0e5d60e1e10f9efc2ad70d7afa0e05dc1d8d89
Nate Collings <nate@youneedabudget.com>
2020-12-22 19:13:04 -0700

    Add the ability to just analyze an already generated FeatureExtraction

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ fish:
 
 docker run -it --rm -v (pwd):/home/majima:delegated --name majima majima
 
+To just analyze an already generated OpenFace extraction:
+
+docker run -it --rm -v (pwd):/home/majima:delegated -e ANALYZE=<out dir name> --name majima majima
+
 Other shells:
 
 docker run -it --rm -v \$(pwd):/home/majima:delegated --name majima majima
--- src/one_eyed_daemon.cr
+++ src/one_eyed_daemon.cr
@@ -2,22 +2,33 @@ require "./blink_detector.cr"
 require "./blink_analyzer.cr"
 require "./logger.cr"
 
-file_types = ["*.mov", "*.mkv", "*.mp4"]
-file_paths = file_types.map { |type| File.join(ENV["MAJIMA_PATH"], "data", "video_in", type) }
+def run_daemon
+  file_types = ["*.mov", "*.mkv", "*.mp4"]
+  file_paths = file_types.map { |type| File.join(ENV["MAJIMA_PATH"], "data", "video_in", type) }
 
-loop do
-  Dir[file_paths].each do |video_path|
-    log("Running OpenFace FeatureExtraction on #{video_path}")
-    feature_extraction_analysis_dir = BlinkDetector.detect(video_path)
+  loop do
+    Dir[file_paths].each do |video_path|
+      log("Running OpenFace FeatureExtraction on #{video_path}")
+      feature_extraction_analysis_dir = BlinkDetector.detect(video_path)
 
-    log("Running analysis on the OpenFace FeatureExtraction written to #{feature_extraction_analysis_dir}")
-    BlinkAnalyzer.new(feature_extraction_analysis_dir).analyze
+      log("Running analysis on the OpenFace FeatureExtraction written to #{feature_extraction_analysis_dir}")
+      BlinkAnalyzer.new(feature_extraction_analysis_dir).analyze
 
-    video_name = video_path.split("/").last
-    new_path = File.join(ENV["MAJIMA_PATH"], "data", "video_out", video_name)
-    log("Moving #{video_name} to #{new_path}")
-    File.rename(video_path, new_path)
+      video_name = video_path.split("/").last
+      new_path = File.join(ENV["MAJIMA_PATH"], "data", "video_out", video_name)
+      log("Moving #{video_name} to #{new_path}")
+      File.rename(video_path, new_path)
+    end
+
+    sleep 2.seconds
   end
+end
 
-  sleep 2.seconds
+if ENV["ANALYZE"]?
+  path = File.join(ENV["MAJIMA_PATH"], "data", "analysis_out", ENV["ANALYZE"])
+
+  log("Running analysis on the OpenFace FeatureExtraction in #{path}")
+  BlinkAnalyzer.new(path).analyze
+else
+  run_daemon
 end
